9017c319919e56053f0572c268281beca08d32f9
Oliver Hawkins <oli@olihawkins.com>
2020-05-15 14:50:42 +0100

    Testing alternative authentication methods

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,4 @@
 .onLoad <- function(libname, pkgname) {
     if (file.exists(".httr-oauth")) googleAnalyticsR::ga_auth(".httr-oauth")
+    if (file.exists(file.path("auth", "auth.json"))) googleAuthR::gar_auth_service(file.path("auth", "auth.json"))
 }
